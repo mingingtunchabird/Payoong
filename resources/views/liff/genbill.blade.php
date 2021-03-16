00383c8b23361226ec93932ac759c2f9fde66679
ming <ming@ming.local>
2021-03-16 17:21:33 +0700

    tes bot22

--- resources/views/liff/genbill.blade.php
+++ resources/views/liff/genbill.blade.php
@@ -2,6 +2,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UPLOAD RECIEPT</title>
