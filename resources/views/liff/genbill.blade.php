8d607616f4db71272a01301c202bb2f731e9b256
ming <ming@ming.local>
2021-03-16 17:07:45 +0700

    tes bot22

--- resources/views/liff/genbill.blade.php
+++ resources/views/liff/genbill.blade.php
@@ -41,7 +41,7 @@
 
 <div class="container-fluid">
   <div class="row">
-  <div class="col-xs-6 col-md-4  mt-4 text-center mr-2"><img src="https://sv1.picz.in.th/images/2021/02/11/o1lNH9.png" width="200" height="45"></div>
+  <div class="col-xs-6 col-md-4  mt-4 "><img src="https://sv1.picz.in.th/images/2021/02/11/o1lNH9.png" width="200" height="45"></div>
   <div class="col-xs-6 col-md-4"><h1 class="mt-5 title" style="margin-top:80px; text-align: center;">อัปโหลดภาพสลิปโอนเงิน</h1></div>
 
 
