606ed3ea7dec3361581578cfc75213a3f2f4a8a9
ming <ming@ming.local>
2021-03-18 10:34:31 +0700

    tes bot25

--- resources/views/liff/genbill.blade.php
+++ resources/views/liff/genbill.blade.php
@@ -4,6 +4,7 @@
 <head>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;400&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UPLOAD RECEIPT</title>
