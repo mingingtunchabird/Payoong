aa0fd5d906896d063bdc8d9c5c315f9123a08e1a
ming <ming@ming.local>
2021-03-18 12:10:40 +0700

    tes bot25

--- resources/views/liff/genbill.blade.php
+++ resources/views/liff/genbill.blade.php
@@ -5,6 +5,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;400&display=swap" rel="stylesheet">
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
+    <script src="liff-starter.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UPLOAD RECEIPT</title>
@@ -54,7 +56,6 @@
         <div class="custom-file rounded-50">
             <input type="file" class="custom-file-input" id="customFile" name="image">
             <label class="custom-file-label rounded-50" for="customFile" style="text-align: center;">Choose image</label>
-            {{-- <img id="blah" src="#" alt="book image" class="preview-img mt-3 rounded-nm"/> --}}
         </div>
       </div>
 
