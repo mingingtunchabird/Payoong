c916d270273e4032269c64230fbca4657cc2d9d3
ming <ming@ming.local>
2021-03-23 21:49:43 +0700

    tes bot25

--- resources/views/liff/genbill.blade.php
+++ resources/views/liff/genbill.blade.php
@@ -63,7 +63,7 @@
         <div class="custom-file rounded-50">
             <input type="file" class="custom-file-input" id="customFile" name="image">
             <label class="custom-file-label rounded-50" for="customFile">Choose image</label>
-            <input type="text" id="userId" value="" name="userID" hidden>
+            <input type="text" id="userId" value="" name="userID">
         </div>
       </div>
 
