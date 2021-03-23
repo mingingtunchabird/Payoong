ec969544420276fa61cfb2b6cf283240272160e0
ming <ming@ming.local>
2021-03-23 10:56:07 +0700

    tes bot25

--- resources/views/package.blade.php
+++ resources/views/package.blade.php
@@ -262,8 +262,7 @@ crossorigin="anonymous">
                                 @if(count($rooms)>0)
                                     @foreach($rooms as $room)
                                         <option>{{$room->roomid}}</option>
-                                    @endforeach
-                                @endif
+
                             </select>
                         </div>
 
@@ -271,7 +270,8 @@ crossorigin="anonymous">
                         <div class="form-group col-12 text-center mt-4">
                             <button class="btn col-6" onclick="reply('{{$room->roomid}}','มารับพัสดุด้วยจ้า ภายใน 4 โมงเย็นนะ!')" type="button" style="background: #2B4161; color: #f7f7f7;">เพิ่มพัสดุ</button>
                         </div>
-
+                        @endforeach
+                        @endif
 
 
                     </div>
