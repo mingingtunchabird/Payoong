844ec0bb1e7895d0d4303861fcffc6b234323575
ming <ming@ming.local>
2021-03-28 22:17:38 +0700

    tes bot22

--- resources/views/liff/genbill.blade.php
+++ resources/views/liff/genbill.blade.php
@@ -45,7 +45,7 @@
 
 </style>
 
-{{-- <input type="text" id="userId" value=" " name="userid"> --}}
+<input type="text" id="userId" value=" " name="userid" hidden>
 
 {{-- <img id="pictureUrl" width="10%" style="border-radius: 50%; float: left;"> --}}
   {{-- <p id="userId"></p> --}}
@@ -84,7 +84,7 @@
       </div>
 
       <div class="col-xs-6 col-md-4 mt-5 text-center d-inline-flex ml-1 p-1" style="margin-top:200px;">
-                <button id="btnSm" style="width:100%; height:50px; border-radius:70px;" onclick="pop()" class="btn btn-primary" type="button" > <p style="font-size:1.2rem; margin-top:3px;">UPLOAD</p></button>
+                <button style="width:100%; height:50px; border-radius:70px;" onclick="pop()" class="btn btn-primary" type="button" > <p style="font-size:1.2rem; margin-top:3px;">UPLOAD</p></button>
         </div>
 
 
@@ -96,6 +96,7 @@
 </div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
