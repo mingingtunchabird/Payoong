ff829d3121a29e9a51ece964872b1d1159aff725
ming <ming@ming.local>
2021-05-09 09:45:11 +0700

    tes bot25

--- resources/views/userHome.blade.php
+++ resources/views/userHome.blade.php
@@ -1,6 +1,8 @@
 
 @extends('layouts.app')
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
 
 @section('content')
 
